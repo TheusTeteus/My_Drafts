d82290d6dbd73d7e11f42553f349dceb7ca94846
Matheus Rocha <102763984+TheusTeteus@users.noreply.github.com>
2023-12-12 12:29:40 -0300

    Update cep_google_sheets_app_script.js

--- cep_google_sheets_app_script.js
+++ cep_google_sheets_app_script.js
@@ -1,68 +1,38 @@
+function pesquisaCEP(){
 
-/* ================== function to manipulate the code ahead (search address) throughtout all active cells ================== */
+var guia = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet()
 
-function onEdit(){
-    
-var current_sheet = SpreadsheetApp.getActive().getSheetName();
+for (var i=0;i<5000;i++){
 
-if(current_sheet == "Página1"){
+var cep = guia.getRange(2+i,1).getValue() 
+var cep = cep.replace(/\D/g, '');
+var validacep = /^[0-9]{8}$/;
 
-var active_cells = SpreadsheetApp.getActive().getSheetByName("Página1");
-var row = active_cells.getActiveCell().getRow();
-var column = active_cells.getActiveCell().getColumn();
+if (cep != ""){
 
-if(row > 1 && column == 1){
-  search_address();
-}
+  if (validacep.test(cep)){
 
-}
+    var url = "http://viacep.com.br/ws/" + cep + "/json/";
+        var retorno = UrlFetchApp.fetch(url);
+        var json = retorno.getContentText();
+        var dados = JSON.parse(json);
 
-}
+        if(dados.logradouro != null){
 
-
-/* =============================== function to make a request on postcode number added ======================================= */
-
-function search_address() {
-  
-var current_sheet = SpreadsheetApp.getActiveSpreadsheet();
-var sheet = current_sheet.getSheetByName("Página1");
-var row = sheet.getActiveCell().getRow();
-var post_code = sheet.getRange(row,1).getValue();
-
-// whether postcode cell is blank, a false boolean aftermath will be given and all cells will be cleared
-    
-if(post_code == ""){
-  sheet.getRange(row,2,1,6).clearContent();
-  return false;
-}
-    
-var post_code = post_code.replace(/\D/g, '');
-var check_post_code = /^[0-9]{8}$/;
-
-// whether postcode cell is filled, it does a fetch request in the url below    
-    
-if(post_code != ""){
-
-  if(check_post_code.test(post_code)){
-
-    var url = "http://viacep.com.br/ws/" + post_code + "/json/";
-    var comeback = UrlFetchApp.fetch(url);
-    var json = comeback.getContentText();
-    var data = JSON.parse(json);
-
-// as the postcode is found, it seeks for the params below aside to "data"
-      
-    if(data.logradouro != null){
-
-        sheet.getRange(row,2).setValue(data.logradouro);
-        sheet.getRange(row,4).setValue(data.complemento);
-        sheet.getRange(row,5).setValue(data.bairro);
-        sheet.getRange(row,6).setValue(data.localidade);
-        sheet.getRange(row,7).setValue(data.uf);
-        
-    }
+            guia.getRange(2 + i, 2).setValue(dados.logradouro);
+            guia.getRange(2 + i, 4).setValue(dados.complemento);
+            guia.getRange(2 + i, 5).setValue(dados.bairro);
+            guia.getRange(2 + i, 6).setValue(dados.localidade);
+            guia.getRange(2 + i, 7).setValue(dados.uf);
+            
+        }
 
   }
+
+
+}
+
+
 }
 
 }
